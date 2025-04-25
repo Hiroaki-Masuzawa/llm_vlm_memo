f1bffaf6cc6e78b3bd3e78972ecea762d4d37fa3
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2025-04-25 17:10:55 +0900

    fix mount dir

--- python_api/run.sh
+++ python_api/run.sh
@@ -1,8 +1,9 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 docker run -it --rm \
     --net=host \
     -u `id -u`:`id -g` \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
-    -v ${PWD}:/userdir -w /userdir \
+    -v ${SCRIPT_DIR}:/userdir -w /userdir \
     --name python_openai \
     python_openai bash
