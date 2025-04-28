823bd0862c4a755f360202564318ef1f1d1be21d
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2025-04-28 20:17:27 +0900

    add bash scrit

---
+++ run_vllm_bash.sh
@@ -0,0 +1,29 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+CACHEDIR=${SCRIPT_DIR}/vllm_root_cache
+GPUOPTION="--gpus all"
+IMAGE_NAME=vllm/vllm-openai:latest
+OPTIONS=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c|--cpu)
+            IMAGE_NAME="vllm-cpu-env"
+            GPUOPTION=" "
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+    esac
+done
+
+docker run -it --rm ${GPUOPTION} \
+  -v ${CACHEDIR}:/root/.cache \
+  -v ${SCRIPT_DIR}/vllm:/vllm \
+  -v ${SCRIPT_DIR}:/userdir -w /userdir \
+  --entrypoint="" ${IMAGE_NAME} bash
