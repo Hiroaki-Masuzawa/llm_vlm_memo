503d000bbeed90554e4fa2d41f440a31f02137f0
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2025-06-24 15:18:54 +0900

    mod code

--- javascript_scripts/index.mjs
+++ javascript_scripts/index.mjs
@@ -1,14 +1,30 @@
 // index.mjs
 import OpenAI from 'openai';
 
+var server_ip = 'localhost'
+var port = '8000'
+var model_name = 'Qwen2-VL-2B-Instruct'
+
+if (process.argv.length > 2) {
+  server_ip = process.argv[2]
+}
+if (process.argv.length > 3) {
+  port = process.argv[3]
+}
+if (process.argv.length > 4) {
+  model_name = process.argv[4]
+}
+
+var request_url = `http://${server_ip}:${port}/v1`
+
 const openai = new OpenAI({
-  baseURL: 'http://'+process.argv[2]+':'+process.argv[3]+'/v1', // 現在使用しているAPIサーバURLに変更
+  baseURL: request_url,
   apiKey: 'ollama', // ダミーでOK（OpenAI風に必須なだけ）
 });
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    model: 'Qwen2-VL-2B-Instruct', // 現在使用している使用モデル名に変更
+    model: model_name,
     messages: [{ role: 'user', content: 'こんにちは、調子はどう？' }],
   });
 
